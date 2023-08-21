00f7a381a4ee415c2e5b281e499dec7afafcda4a
Fenneig <ninetailedkot92@gmail.com>
2023-08-21 20:42:55 +0300

    Audio management + persistent properties

---
+++ Assets/Scripts/Audio/AudioSettingsComponent.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Flappybird.Model.Data;
+using Flappybird.Model.Data.Properties;
+using UnityEngine;
+
+namespace Flappybird.Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioSettingsComponent : MonoBehaviour
+    {
+        [SerializeField] SoundSettings _mode;
+        private AudioSource _source;
+        private FloatPersistentProperty _model;
+
+        private void Awake()
+        {
+            _source = GetComponent<AudioSource>();
+            _model = FindProperty();
+            _model.OnChanged += OnSoundSettingsChange;
+
+            OnSoundSettingsChange(_model.Value, _model.Value);
+        }
+
+        private void OnSoundSettingsChange(float newValue, float _)
+        {
+            _source.volume = newValue;
+        }
+
+        private FloatPersistentProperty FindProperty()
+        {
+            switch (_mode)
+            {
+                case SoundSettings.Music: return GameSettings.I.Music;
+                case SoundSettings.Effects: return GameSettings.I.Effects;
+            }
+            throw new ArgumentException($"Undefined argument! Check if you forget to change Sound settings mode from Master on {gameObject.name} object");
+        }
+
+        private void OnDestroy()
+        {
+            _model.OnChanged -= OnSoundSettingsChange;
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/UI/AudioSettingsUIComponent.cs
@@ -0,0 +1,18 @@
+﻿using Flappybird.Model.Data;
+using UnityEngine;
+
+namespace Flappybird.Menus.UI
+{
+    public class AudioSettingsUIComponent : MonoBehaviour
+    {
+        [Header("Widgets")]
+        [SerializeField] private AudioSettingsWidget _musicWidget;
+        [SerializeField] private AudioSettingsWidget _effectsWidget;
+
+        private void Awake()
+        {
+            _musicWidget.SetModel(GameSettings.I.Music);
+            _effectsWidget.SetModel(GameSettings.I.Effects);
+        }
+    }
+}
